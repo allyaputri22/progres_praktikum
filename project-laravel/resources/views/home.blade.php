fb2772ba36f9b0bd2d8421d8b37cb68dc456b92c
Allya Putri <liyaliyu293@gmail.com>
2024-10-03 12:45:35 +0700

    Pertemuan 4

---
+++ project-laravel/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('title','Home Page')
+
+@section('content')
+<h1>Welcome To My Page</h1>
+
+@endsection
--- project-laravel/routes/web.php
+++ project-laravel/routes/web.php
@@ -2,14 +2,34 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+
+Route::get('/product',[ProductController::class, 'index']);
+// Route::get('/product/create',[ProductController::class, 'create']);
+// Route::post('/product',[ProductController::class, 'store']);
+// Route::get('/product/{id}',[ProductController::class, 'show']);
+// Route::get('/product/{id}/edit',[ProductController::class, 'edit']);
+// Route::put('/product/{id}',[ProductController::class, 'update']);
+// Route::delete('/product/{id}',[ProductController::class, 'destroy']);
+
+//Route::resource(name: 'product', controller: ProductController::class);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// // Route::get('/product', function () {
+// //     return view('product');
+// });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified','RoleCheck:admin'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
